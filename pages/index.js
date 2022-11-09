6716014ae59f91dc88d420f119e3dc5dcaff14d6
Aaron Kaye <ajk292@cornell.edu>
2022-11-08 20:36:05 -0500

    Adding Prisma, adding a first schema for homes, and rendering homes on index page.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
 import Layout from '../components/layouts/Layout';
+import prisma from '../lib/prisma';
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const homes = await prisma.home.findMany();
+  return {
+    props: { homes },
+    revalidate: 10,
+  };
+};
+
+export default function Home({ homes }) {
   return (
     <Layout>
       <h1 className="text-3xl bold underline">All homes</h1>
+      {homes.map((home, i) => (
+        <div key={i}>
+          <p>{home.title}</p>
+          <ul className="list-disc ml-5">
+            <li>{home.location}</li>
+            <li>{home.pricePerNight}</li>
+          </ul>
+        </div>
+      ))}
     </Layout>
   );
 }
